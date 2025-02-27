5327e4ef00bce97db71a81d324655d12d3bd0304
tanulo <tanulo@barossgabor.local>
2025-02-27 09:11:43 +0100

    Keyup

--- script.js
+++ script.js
@@ -12,10 +12,22 @@ window.addEventListener('DOMContentLoaded', () => {
     textarea.name = 'area';
     textarea.id = 'area';
     textarea.placeholder = 'Írd be a jelszavad:';
+    textarea.addEventListener('keyup', () => jelszo(textarea, demoSpan));
 
-    containerDiv.append(demoSpan, textarea);
+    containerDiv.append(demoSpan, textarea)
 
-    document.body.append(containerDiv); 
+    document.body.append(containerDiv)
 
-    console.log(containerDiv, textarea);
+
 });
+
+
+
+function jelszo(textarea, demoSpan){
+    let csillag = '';
+
+    for(let i = 0; i < textarea.value.length; i++){
+        csillag += '*'
+    }
+    demoSpan.textContent = csillag;
+};
